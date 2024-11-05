7eb215451cbda04afb8bebc949331e65bef94611
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-11-05 14:51:38 +0700

    admin page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast";
 import { useUserStore } from "./stores/useUserStore";
 import { useEffect } from "react";
 import LoadingSpinner from "./components/LoadingSpinner";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   const { user, checkAuth, checkingAuth } = useUserStore();
@@ -41,6 +42,16 @@ function App() {
           <Route
             path="/signup"
             element={user ? <Navigate to={"/"} /> : <SignUpPage />}
+          />
+          <Route
+            path="/secret-dashboard"
+            element={
+              user?.role === "admin" ? (
+                <AdminPage />
+              ) : (
+                <Navigate to={"/login"} />
+              )
+            }
           />
         </Routes>
       </div>
